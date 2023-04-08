4ca328103ffb4e8bca673e8f94209f602e961953
DevpSchneider <brunocc31@gmail.com>
2023-04-08 20:07:22 -0300

    programa.5

---
+++ programa5.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+    int main()
+    
+            {
+        
+                int b, h;
+                printf("base:\n");
+                scanf("%d", &b);
+                printf("altura:\n");
+                scanf("%d", &h);
+                int a = b*h;
+                printf("area do retangulo:%d\n", a);
+
+                return 0;
+            
+
+            }
+    
