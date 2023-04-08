ab5576d9f1248724bbad8e783d6a1ac63f81c0bb
DevpSchneider <brunocc31@gmail.com>
2023-04-08 20:07:39 -0300

    programa.6

---
+++ programa6.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+    int main()
+    
+            {
+        
+                int a, b, c;
+                printf("numero 1:\n");
+                scanf("%d", &a);
+                printf("numero 2:\n");
+                scanf("%d", &b);
+                printf("numero 3:\n");
+                scanf("%d", &c);
+                int d = (a + b + c)/2;
+                printf("Media:%d\n", d);
+
+                return 0;
+            
+
+            }
+    
+
+
+
+    
